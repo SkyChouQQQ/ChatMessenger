26cf03513314b2267fa5e375868fe85e0dd254d1
Sky <skyc2298@gmail.com>
2018-12-05 22:54:29 +0800

    init commit

---
+++ Chaat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chaat
+//
+//  Created by Admin on 2018/12/5.
+//  Copyright © 2018 Sky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
