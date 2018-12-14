b332652c3231c2a0f9a494288c77e96edb0be981
Sky <skyc2298@gmail.com>
2018-12-15 03:32:16 +0800

    add UIColor extension

--- Chaat/Helpers/Extensions.swift
+++ Chaat/Helpers/Extensions.swift
@@ -74,3 +74,10 @@ extension AVAsset{
         
     }
 }
+
+extension UIColor {
+    static func rgb(r: CGFloat, g: CGFloat, b: CGFloat)->UIColor {
+        return UIColor(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+    
+}
