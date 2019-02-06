beed5a8051950af5f39747cb91cf83b3e76da2f2
Sky <skyc2298@gmail.com>
2019-02-06 23:20:32 +0800

    set up newsFeedVC userProfileVC in mainTabBarVC

---
+++ Chaat/Controller/NewsFeedController.swift
@@ -0,0 +1,9 @@
+//
+//  NewsFeedController.swift
+//  Chaat
+//
+//  Created by Admin on 2019/2/6.
+//  Copyright © 2019 Sky. All rights reserved.
+//
+
+import Foundation
---
+++ Chaat/Controller/UserProfileController.swift
@@ -0,0 +1,9 @@
+//
+//  UserProfileController.swift
+//  Chaat
+//
+//  Created by Admin on 2019/2/6.
+//  Copyright © 2019 Sky. All rights reserved.
+//
+
+import Foundation
